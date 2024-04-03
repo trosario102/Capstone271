d3c76e0532991f1f09e986194ef3cad63b310871
mkhursiwala2005 <150825698+mkhursiwala2005@users.noreply.github.com>
2024-04-03 13:11:45 -0500

    Add files via upload

---
+++ PersonGroceryStoreGroceryListTester.java
@@ -0,0 +1,106 @@
+import java.util.Scanner;
+
+public class PersonGroceryStoreGroceryListTester {
+    public static void main(String[] args) {
+        GroceryItem item1 = new GroceryItem("apple", 0.68);
+        GroceryItem item2 = new GroceryItem("strawberry", 2.11);
+        GroceryItem item3 = new GroceryItem("eggs", 3.88);
+        GroceryItem item4 = new GroceryItem("bread", 3.79);
+        GroceryItem item5 = new GroceryItem("milk", 4.99);
+        GroceryItem item6 = new GroceryItem("tomatoes", 1.19);
+        GroceryItem item7 = new GroceryItem("cucumber", 1.00);
+        GroceryItem item8 = new GroceryItem("olives", 6.99);
+        GroceryItem item9 = new GroceryItem("avocado", 1.99);
+        GroceryItem item10 = new GroceryItem("lettuce", 1.99);
+        GroceryItem item11 = new GroceryItem("pasta", 5.93);
+        GroceryItem item12 = new GroceryItem("blueberry", 3.39);
+        GroceryItem item13 = new GroceryItem("orange", 0.98);
+        GroceryItem item14 = new GroceryItem("lemon", 0.99);
+
+        GroceryStoresLists storesLists = new GroceryStoresLists();
+
+        GroceryStore store1 = new GroceryStore("Mariano's");
+        storesLists.addStore(store1);
+        store1.add(item5);
+        store1.add(item12);
+        store1.add(item4);
+        store1.add(item8);
+        store1.add(item6);
+        store1.add(item7);
+        store1.add(item11);
+        store1.add(item1);
+        store1.add(item13);
+        store1.add(item14);
+        store1.add(item3);
+
+        GroceryStore store2 = new GroceryStore("Target");
+        storesLists.addStore(store2);
+        store2.add(item2);
+        store2.add(item3);
+        store2.add(item9);
+        store2.add(item10);
+        store2.add(item13);
+        store2.add(item14);
+        store2.add(item5);
+        store2.add(item12);
+        store2.add(item11);
+        store2.add(item4);
+
+        Person p1 = new Person("Mohammed", "Khursiwala");
+        Person p2 = new Person("Mike", "James");
+        Person p3 = new Person("Aishah", "Raquib");
+        Person p4 = new Person("Umayma", "Raquib");
+
+        Professor prof = new Professor(p2.getFirstName(), p2.getFamilyName(), "Comp", "Data Structures I");
+        Undergraduate undergrad = new Undergraduate(p1.getFirstName(), p1.getFamilyName(),512172, "freshmen");
+        Graduate grad = new Graduate(p3.getFirstName(), p3.getFamilyName(), 567128,"May 2025");
+        AtLarge at = new AtLarge(p4.getFirstName(), p4.getFamilyName(), 514766);
+
+        PersonRegistry registry = new PersonRegistry();
+        registry.addPerson(prof);
+        registry.addPerson(undergrad);
+        registry.addPerson(grad);
+
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to Grocery Shopping App!");
+        System.out.print("Enter your first & last name: ");
+        String firstName = scan.next();
+        String lastName = scan.next();
+
+        System.out.println("Welcome " + registry.getPerson(firstName, lastName));
+        System.out.print("You have no grocery lists saved. Type your Grocery Store: ");
+        String store = scan.next();
+
+        while (!registry.selectGroceryStore(store, storesLists)) {
+            store = scan.next();
+        }
+
+        System.out.print("I can shop at " + store + " Would you want to shop right now? ");
+        String answer = scan.next();
+
+        while (!answer.equals("ok")) {
+            answer = scan.next();
+        }
+        GroceryStore selectStore = storesLists.getGroceryStore(store);
+        System.out.println(selectStore);
+
+        GroceryList groceryList = new GroceryList();
+
+        System.out.print("Enter your items one at a time and I'll give you a price if it's available ");
+        String item = scan.next();
+
+        while (!item.equals("stop")) {
+            System.out.print(item + ", Quantity? ");
+            int quantity = scan.nextInt();
+            registry.addGroceryItems(item, quantity, selectStore, groceryList);
+            System.out.println("Next Item? ");
+            item = scan.next();
+        }
+
+        System.out.println(groceryList);
+        double discountedPrice = registry.checkPersonDiscount(new Person(firstName, lastName), undergrad, grad, groceryList);
+        System.out.println("Total after discount applied: " + discountedPrice);
+    }
+}
---
+++ StudentCourseWaitlistsTester.java
@@ -0,0 +1,43 @@
+public class StudentCourseWaitlistsTester {
+    public static void main(String[] args) {
+
+
+        Student s1 = new Student("Mohammed", "Khursiwala", 512172);
+        Student s2 = new Student("John", "Johnson", 509221);
+        Student s3 = new Student("Mary", "John",543491);
+        Student s4 = new Student("Mark", "Mellon", 578923);
+        Student s5 = new Student("Tom", "Mellon",571122);
+        Student s6 = new Student("Mohammed", "Ahmed",567458);
+        Student s7 = new Student("Aishah", "Raquib", 568799);
+        Student s8 = new Student("Umayma", "Raquib", 531456);
+        Student s9 = new Student("Harry", "Berry", 573764);
+
+        Course c1 = new Course("Comp", 271, "Data Structures I", 3);
+        Course c2 = new Course("Math", 212, "Linear Algebra", 3);
+        Course c3 = new Course("Stat", 203, "Intro to Statistics", 3);
+
+
+        s1.addCourse(c1);
+        s1.addCourse(c3);
+        s1.addCourse(c2);
+
+        s2.addCourse(c1);
+        s3.addCourse(c1);
+        s4.addCourse(c1);
+        s5.addCourse(c1);
+        s6.addCourse(c1);
+        s7.addCourse(c1);
+        s8.addCourse(c1); // will be on wait list of Comp 271 course
+        s9.addCourse(c1); // will be on the wait list of Comp 271 course
+
+        System.out.println(s1);
+
+        s2.dropCourse(c1);
+
+        CourseRegistry courseRegistry = new CourseRegistry(c1);
+        System.out.println("Registered Students in " + c1.getCourseTitle() +  "\n" + courseRegistry.findRegisteredStudents(c1));
+        System.out.println("WaitListed Students in " + c1.getCourseTitle() + "\n" + courseRegistry.findWaitListStudents(c1));
+        System.out.println("Mohammed Khursiwala in Comp 271: " + courseRegistry.contains(s1, c1)); // true
+        System.out.println("Does Mohammed Khursiwala & John Johnson are in the same course? " + courseRegistry.equals(s1, s2, c1)); // true
+    }
+}
