01223b5ce3100ea314547bea7e17f5634f3031d2
trosario102 <t.rosario102@gmail.com>
2024-04-12 00:10:09 -0500

    added children stuff to main test file. fixed some other things

--- User.java
+++ User.java
@@ -4,5 +4,5 @@ public interface User {
     // public boolean hasGroceryList(String listName); // returns true if a grocery list exists for a person else return false
     // public List<GroceryItemOrder> getGroceryList(String listName);
     public void addGroceryItems(String item, int quantity, GroceryStore store, GroceryList list);
-    public double checkPersonDiscount(Person person, Undergraduate undergrad, Graduate grad, GroceryList list); // checks if person is professor, or undergrad, or grad, or atLarge to implement percent discount behavior
+    public double checkPersonDiscount(Person person, GroceryList list); // checks if person is professor, or undergrad, or grad, or atLarge to implement percent discount behavior
 }
